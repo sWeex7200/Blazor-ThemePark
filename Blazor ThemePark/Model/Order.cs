f4fe1b94c932e07a24f7cd8fccd4a90c030a45c3
Jarne Staes <jarnestaes@hotmail.com>
2023-10-31 09:37:17 +0100

    add confirmation page + class Order and OrderDetails

---
+++ Blazor ThemePark/Model/Order.cs
@@ -0,0 +1,13 @@
+﻿using static Blazor_ThemePark.Pages.Stores.ConfirmOrder;
+
+namespace Blazor_ThemePark.Model
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public OrderDetails UserDetails { get; set; } = null!;
+        public List<ShopItem> Items { get; set; } = null!;
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ Blazor ThemePark/Model/OrderDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_ThemePark.Model
+{
+    public class OrderDetails
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
